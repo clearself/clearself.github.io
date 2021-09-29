d647244e51d58b33c93dc2eca449a4dce585352c
clearself <fuleiyang@qq.com>
2021-09-29 15:25:44 +0800

    修改部署6

---
+++ subapp/sub-react/precache-manifest.3c61f5859baa44ce3d7248f4404a49ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d44e712641eee1812e314f6d61c4d4c",
+    "url": "/subapp/sub-react/index.html"
+  },
+  {
+    "revision": "cde452a549e050df522d",
+    "url": "/subapp/sub-react/static/css/main.8c153d05.chunk.css"
+  },
+  {
+    "revision": "81eab3564cc7dc2ec86a",
+    "url": "/subapp/sub-react/static/js/2.ab785b48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/subapp/sub-react/static/js/2.ab785b48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde452a549e050df522d",
+    "url": "/subapp/sub-react/static/js/main.6f147d44.chunk.js"
+  },
+  {
+    "revision": "935ca0651b0109d01f76",
+    "url": "/subapp/sub-react/static/js/runtime-main.09e55165.js"
+  }
+]);
--- subapp/sub-react/service-worker.js
+++ subapp/sub-react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1e866b5c9945a73f8420194d1dee0050.js"
+  "/subapp/sub-react/precache-manifest.3c61f5859baa44ce3d7248f4404a49ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/subapp/sub-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
